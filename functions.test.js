ee233b9155b987b2cd4ed719e3df4005d9144bdf
campuzan21 <45133099+campuzan21@users.noreply.github.com>
2019-01-24 21:34:24 -0800

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -3,3 +3,7 @@ const sum = require('./functions');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+test('adds 1 + 2 to equal 4', () => {
+  expect(sum(1, 2)).toBe(4);
+});
